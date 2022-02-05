2c47375cacb44f070d73c612710f9a916df493a4
Sumit Kumar <sumit0001.jnv@gmail.com>
2022-02-05 10:50:56 +0530

    video size changed

--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import Header from '../../components/header/Header';
+import Box from '@mui/material/Box';
 
 
 export default function Home() {
     return (<>
         <Header></Header>
-        <video sx={{
-            margin: 4,
-            padding: 4
-        }} id="background-video" autoPlay loop muted>
-            <source src="https://s3.ap-south-1.amazonaws.com/www.einsite.io/img/einsitewebsiteloop.mp4" type="video/mp4" />
-            <source src="https://s3.ap-south-1.amazonaws.com/www.einsite.io/img/einsitewebsiteloop.mp4" type="video/ogg" />
-            Your browser does not support the video tag.
-        </video>
+            <video style={{
+                width: "100%"
+            }} id="background-video" autoPlay loop muted>
+                <source src="videos/home-video.mp4" type="video/mp4" />
+                <source src="videos/home-video.mp4" type="video/ogg" />
+                Your browser does not support the video tag.
+            </video>
     </>
     )
 }
